b961c4c23a321459b31c4b5c81daf699df1521fd
sunnykuo1122 <sunnykuo@mak66design.com>
2023-07-21 16:09:26 +0800

    更新頁碼

--- local/index.php
+++ local/index.php
@@ -165,6 +165,16 @@
                     </a>
                 </li>
             </ul>
+            <dl class="page">
+                <!-- 若沒有上一則或下一則請使用 nopage -->
+                <dt class="ltbn nopage arrow"><a href="javascript:;"><img src="dist/svg/arrowL.svg" class="svg"></a></dt>
+                <dd><a href="#">1</a></dd>
+                <dd class="active"><a href="#">2</a></dd>
+                <dd><a href="#">3</a></dd>
+                <dd><a href="#">4</a></dd>
+                <dd><a href="#">5</a></dd>
+                <dt class="rtbn arrow"><a href="javascript:;"><img src="dist/svg/arrowR.svg" class="svg"></a></dt>
+            </dl>
             <div class="dres">
                 <div class="wave"><img src="dist/images/news_wave.png"></div>
                 <div class="boat"><img src="dist/images/news_boat.png"></div>
